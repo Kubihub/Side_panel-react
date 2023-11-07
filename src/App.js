b3aba145af112749a3b12d3bec7c2bf6b9fa97b3
Kubihub <kuburatuadiza5@gmail.com>
2023-11-07 15:50:16 +0000

    Dashboard navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import {BsFillArrowLeftCircleFill} from "react-icons/bs";
+// import { Nav } from "./components/Nav"
 
-function App() {
+const App =()=>{
+
+  const[open, setOpen]=useState(true);
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    {/* <Nav/> */}
+    {/* <div className="bg-600 w-full h-screen"> */}
+    <div className="flex">
+      <div className={`bg-dark-blue h-screen p-5 pt-8 ${open? "w-72":"w-20"} duration-300 relative`}>
+      <BsFillArrowLeftCircleFill className="bg-white text-dark-purple text-3xl rounded-full absolute-right top-9 mx-60 bg-white cursor-pointer
+      border light-white" onClick={()=> setOpen(!open)}
+      />
+      </div>
+      <div className="p-7">
+        <h1 className="text-2xl font-semibold">Homepage</h1>
+      </div>
+      </div>
+    {/* </div> */}
+    </>
+  )
 }
-
-export default App;
+export default App
---
+++ src/components/Nav.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Nav = () => {
+  return (
+    <div className='shadow-md w-full fixed top-0 left-0'>
+        <div className='md:flex by-white py-3'>
+            <div>
+                <span>
+                    <img src="logo192.png" alt="" width={30} height={30}/>
+                </span>
+            </div>
+        </div>
+
+    </div>
+  )
+}
