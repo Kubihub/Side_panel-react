511c7465a504b7cc5dfec04c65b81913da019596
Kubihub <kuburatuadiza5@gmail.com>
2023-11-08 11:44:25 +0000

    Insertion of logo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { AiOutlineAudit } from "react-icons/ai";
 import {BsFillArrowLeftCircleFill} from "react-icons/bs";
  import { Nav } from "./components/Nav"
 
@@ -11,17 +12,24 @@ const App =()=>{
   return (
     <>
      <Nav/> 
-     <div className="bg-600 w-full h-screen"> 
-      <div className={`bg-dark-blue h-screen p-5 pt-8 ${open? "w-72":"w-20"} duration-300 relative`}>
+     <div className="bg-600 w-full h-screen flex"> 
+      <div className={`bg-dark-blue h-screen p-5 pt-8 ${open? "w-72":"w-20"} duration-300 relative`}
+      
+      >
       <BsFillArrowLeftCircleFill className="bg-light-white text-3xl rounded-full absolute -right-3 top-9 cursor-pointer
       border-dark-blue" onClick={()=> setOpen(!open)}
-      
       />
+      <div className="inline-fles">
+        <AiOutlineAudit className="bg-amber-300 text-4xl
+        rounded cursor-pointer block float-left mr-2"/>
+        <h1 className={`text-white origin-left font-medium
+        text-2xl duration-300 ${!open && "scale-0"}`}>GSET</h1>
+      </div>
+
+      </div>
       <div className="p-7">
         <h1 className="text-2xl font-semibold">Homepage</h1>
       </div>
-      </div>
-      
       </div>
     
     </>
