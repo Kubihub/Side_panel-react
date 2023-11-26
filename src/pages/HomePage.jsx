8f1e1d0afa061f2ef67e5937762c7f4af87b029b
Sosu Alfred <sosualfred@gmail.com>
2023-11-26 13:17:24 +0000

    place shadow and padding around dashboard children

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,8 +4,10 @@ import DashboardContainer from "../components/DashboardContainer";
 export default function HomePage() {
   return (
     <DashboardContainer>
-      <h1 className="text-2xl font-semibold">Homepage</h1>
-      <img className="h-20 object-cover mt-3" src="./img.jpg" alt="logo" />
+      <div className="bg-white p-4 shadow">
+        <h1 className="text-2xl font-semibold">Homepage</h1>
+        <img className="w-full h-20 object-cover mt-3" src="./img.jpg" alt="logo" />
+      </div>
     </DashboardContainer>
   );
 }
