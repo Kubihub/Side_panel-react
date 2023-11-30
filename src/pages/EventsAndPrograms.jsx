3e36534bd37cde2c80d5364edeba0079ac55a541
Kubihub <kuburatuadiza5@gmail.com>
2023-11-30 13:40:44 +0000

    Editing of events and programs page

--- src/pages/EventsAndPrograms.jsx
+++ src/pages/EventsAndPrograms.jsx
@@ -11,7 +11,7 @@ export default function EventsAndPrograms() {
     {
       title: "Workshop on React",
       desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.",
-      image: "https://picsum.photos/200/300",
+      image: "./img.jpg",
     },
     {
       title: "Angular Workshop",
@@ -38,9 +38,7 @@ export default function EventsAndPrograms() {
                 <p className="text-gray-700 text-base">{event.desc}</p>
               </div>
               <div className="px-6 pt-4 pb-2">
-                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                  #photography
-                </span>
+               
                 <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
                   Button
                 </button>
