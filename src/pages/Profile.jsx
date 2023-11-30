14ed321d4e7dbefb95d050ca254721e05c0558f3
Sosu Alfred <sosualfred@gmail.com>
2023-11-30 12:56:15 +0000

    remove max-w-2xl from profile layout

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,82 +4,75 @@ import DashboardContainer from "../components/DashboardContainer";
 export default function Profile() {
   return (
     <DashboardContainer>
-      {/* This content is supposed to be pulled out from the database that contains the registeration of the users  */}
-
-      <div className="bg-white p-4 shadow p-5">
-      <div className="bg-white max-w-2xl shadow overflow-hidden sm:rounded-lg ">
-  <div className="px-4 py-5 sm:px-6">
-    <h3 className="text-lg leading-6 font-medium text-gray-900">Profile Page</h3>
-    <p className="mt-1 max-w-2xl text-sm text-gray-500">
-      Details and informations about GSET Members
-    </p>
-  </div>
-  <div className="border-t border-gray-200">
-    <dl>
-    <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-        <dt className="text-sm font-medium text-gray-500">
-          First Name
-        </dt>
-        <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-          Adiza
-        </dd>
+      {/* This content is supposed to be pulled out from the database that contains the registration of the users  */}
+      <div className="bg-white shadow p-5">
+        <div className="bg-white shadow overflow-hidden sm:rounded-lg ">
+          <div className="px-4 py-5 sm:px-6">
+            <h3 className="text-lg leading-6 font-medium text-gray-900">
+              Profile Page
+            </h3>
+            <p className="mt-1 text-sm text-gray-500">
+              Details and informations about GSET Members
+            </p>
+          </div>
+          <div className="border-t border-gray-200">
+            <dl>
+              <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt className="text-sm font-medium text-gray-500">
+                  First Name
+                </dt>
+                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                  Adiza
+                </dd>
+              </div>
+              <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt className="text-sm font-medium text-gray-500">Last Name</dt>
+                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                  Alhassan
+                </dd>
+              </div>
+              <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt className="text-sm font-medium text-gray-500">Address</dt>
+                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                  131 Sunflower Hub
+                </dd>
+              </div>
+              <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt className="text-sm font-medium text-gray-500">
+                  Email address
+                </dt>
+                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                  Adi@example.com
+                </dd>
+              </div>
+              <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt className="text-sm font-medium text-gray-500">
+                  Nationality
+                </dt>
+                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                  Ghanaian
+                </dd>
+              </div>
+              <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt className="text-sm font-medium text-gray-500">
+                  Date of Birth
+                </dt>
+                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                  20/03/1988
+                </dd>
+              </div>
+              <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt className="text-sm font-medium text-gray-500">About</dt>
+                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                  To get social media testimonials like these, keep your
+                  customers engaged with your social media accounts by posting
+                  regularly yourself
+                </dd>
+              </div>
+            </dl>
+          </div>
+        </div>
       </div>
-      <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-        <dt className="text-sm font-medium text-gray-500">
-          Last Name
-        </dt>
-        <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-        Alhassan
-        </dd>
-      </div>
-      <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-        <dt className="text-sm font-medium text-gray-500">
-          Address
-        </dt>
-        <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-         131 Sunflower Hub
-        </dd>
-      </div>
-      <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-        <dt className="text-sm font-medium text-gray-500">
-          Email address
-        </dt>
-        <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-          Adi@example.com
-        </dd>
-      </div>
-      <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-        <dt className="text-sm font-medium text-gray-500">
-          Nationality
-        </dt>
-        <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-          Ghanaian
-        </dd>
-      </div>
-      <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-        <dt className="text-sm font-medium text-gray-500">
-          Date of Birth
-        </dt>
-        <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-          20/03/1988
-        </dd>
-      </div>
-      <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-        <dt className="text-sm font-medium text-gray-500">
-          About
-        </dt>
-        <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-          To get social media testimonials like these, keep your customers engaged with your social media accounts by posting regularly yourself
-        </dd>
-      </div>
-    </dl>
-  </div>
-</div>
-        
-      </div>
-     
-
-
     </DashboardContainer>
   );
 }
