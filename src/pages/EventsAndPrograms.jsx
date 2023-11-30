69e8e81d5850934eab6aff48d70391f434b8fa2c
Kubihub <kuburatuadiza5@gmail.com>
2023-11-30 10:24:53 +0000

    added cards to the events page

--- src/pages/EventsAndPrograms.jsx
+++ src/pages/EventsAndPrograms.jsx
@@ -4,13 +4,58 @@ import DashboardContainer from "../components/DashboardContainer";
 export default function EventsAndPrograms() {
   return (
     <DashboardContainer>
-      <div className="bg-white p-4 shadow">
+      <div className="bg-white p-4 ">
       <h1 className="text-2xl font-semibold">Events And Programs</h1>
-        <img className="w-full h-20 object-cover mt-3" src="./img.jpg" alt="logo" />
+        
         <div>
           
-        <div className="max-w-sm rounded overflow-hidden shadow-lg">
-  <img className="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains" />
+        <div className="max-w-sm rounded overflow-hidden shadow-lg flex-box">
+  <img className="w-full" src="./img.jpg" alt="Sunset in the mountains" />
+  <div className="px-6 py-4">
+    <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+    <p className="text-gray-700 text-base">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+    </p>
+  </div>
+  <div className="px-6 pt-4 pb-2">
+    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+  </div>
+</div>
+
+<div className="max-w-sm rounded overflow-hidden shadow-lg">
+  <img className="w-full" src="./img.jpg" alt="Sunset in the mountains" />
+  <div className="px-6 py-4">
+    <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+    <p className="text-gray-700 text-base">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+    </p>
+  </div>
+  <div className="px-6 pt-4 pb-2">
+    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+  </div>
+</div>
+
+<div className="max-w-sm rounded overflow-hidden shadow-lg">
+  <img className="w-full" src="./img.jpg" alt="Sunset in the mountains" />
+  <div className="px-6 py-4">
+    <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+    <p className="text-gray-700 text-base">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+    </p>
+  </div>
+  <div className="px-6 pt-4 pb-2">
+    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+  </div>
+</div>
+
+<div className="max-w-sm rounded overflow-hidden shadow-lg">
+  <img className="w-full" src="./img.jpg" alt="Sunset in the mountains" />
   <div className="px-6 py-4">
     <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
     <p className="text-gray-700 text-base">
